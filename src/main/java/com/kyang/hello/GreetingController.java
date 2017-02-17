dc9034934dc1c45478cb6d4ecef2d1ef403ee8c5
Yang, Kevin <Kevin_Yang@intuit.com>
2017-02-17 09:53:56 -0800

    fix

--- src/main/java/com/kyang/hello/GreetingController.java
+++ src/main/java/com/kyang/hello/GreetingController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
-asdfasfd
 @RestController
 public class GreetingController {
 
